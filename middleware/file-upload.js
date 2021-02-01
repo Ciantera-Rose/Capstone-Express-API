55ced26e073f101210dd1f39a1ac86a707608e81
Ciantera-Rose <ciantera.rose@gmail.com>
2021-02-01 18:44:01 -0500

    add update disk storage config, add filename key and get ext of file type, cb and get unique id with file ext, add destination key for file storage, pass in args, cb and add path to store data

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,10 +1,20 @@
 const multer = require("multer");
+const uuid = require("uuid").v1;
+
+const fileTypes = {
+  "image/jpeg": "jpeg",
+  "image/png": "png",
+  "image/jpg": "jpg",
+};
 
 const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
-    destination: () => {},
-    filename: () => {},
+    destination: (req, file, cb) => {},
+    filename: (req, file, cb) => {
+      const extension = fileTypes[file.mimetype];
+      cb(null, uuid() + "." + extension);
+    },
   }),
 });
 
