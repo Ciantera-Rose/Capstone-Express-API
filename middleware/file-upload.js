9ba3871d7e000ac8836641e4e285b3b4e41cc572
Ciantera-Rose <ciantera.rose@gmail.com>
2021-02-01 18:56:24 -0500

    add fileFilter to control file types, add null and undefined as false and correct filetype to true, add error check for file valid and file not valid, cb error or accept file

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -10,12 +10,19 @@ const fileTypes = {
 const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {},
+    destination: (req, file, cb) => {
+      cb(null, "uploads/images");
+    },
     filename: (req, file, cb) => {
       const extension = fileTypes[file.mimetype];
       cb(null, uuid() + "." + extension);
     },
   }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!fileTypes[file.mimetype];
+    let error = isValid ? null : new Error("Invalid file type");
+    cb(error, isValid);
+  },
 });
 
 module.exports = fileUpload;
